809cbc3a672da442a8df0b135237eea2398e0194
白色の物语 <42845041+Whitestorys@users.noreply.github.com>
2021-06-20 16:07:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,8 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+# Modify default THEME
+sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/g' ./feeds/luci/collections/luci/Makefile
+# Modify default PASSWORD
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF/$1$j/ju0Qm2$Jqi2hHGjEu8Y0xq03ydS7/g' ./package/lean/default-settings/files/zzz-default-settings
