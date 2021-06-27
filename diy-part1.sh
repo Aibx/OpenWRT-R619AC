f7a281a03602dcd90c2c623a78f8398586b0babc
白色の物语 <42845041+Whitestorys@users.noreply.github.com>
2021-06-28 00:28:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git Whitestorys https://github.com/Aibx/openwrt-packages' feeds.conf.default
+sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
 
